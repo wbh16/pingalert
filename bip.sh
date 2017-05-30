d587bdf393d36d98b23d10e19549148175f96c6e
wbh16 <wbhenriques@gmail.com>
2017-05-30 17:30:22 -0300

    Create bip.sh
    Arquivos com os sons que serão tocados com o programa beep

---
+++ bip.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+# Script base de Marcos Miras
+# Editado por Wemerson Bruno
+
+clear
+echo "Você deve ter o beep instalado e um buzzer em sua placa mãe!" && echo && echo && echo
+echo "Playlist:"
+
+bip1(){
+echo "Do, re, mi, fa"
+beep -f 261.6 && beep -f 293.7 && beep -f 329.6 && beep -f 349.2 && beep -f 349.2 && beep -f 349.2 -l 800 && beep -f 261.6 && beep -f 293.7 && beep -f 261.6 && beep -f 293.7 && beep -f 293.7 && beep -f 293.7 -l 800 && beep -f 261.6 && beep -f 392 && beep -f 349.2 && beep -f 329.6 && beep -f 329.6 && beep -f 329.6 -l 800 && beep -f 261.6 && beep -f 293.7 && beep -f 329.6 && beep -f 349.2 && beep -f 349.2 && beep -f 349.2 -l 800
+}
+
+bip2(){
+echo "Pretty Woman"
+beep -f 329.6
+beep -f 329.6
+beep -f 415.3
+beep -f 523.2
+beep -f 587.4 -l 1000
+beep -f 329.6
+beep -f 329.6
+beep -f 415.3
+beep -f 523.2
+beep -f 587.4 -l 1000
+beep -f 329.6
+beep -f 329.6
+beep -f 415.3
+beep -f 523.2
+beep -f 587.4
+beep -f 784
+beep -f 698.4
+beep -f 587.4
+beep -f 329.6
+beep -f 329.6
+beep -f 415.3
+beep -f 523.2
+beep -f 587.4
+beep -f 784
+beep -f 698.4
+beep -f 587.4
+beep -f 329.6
+beep -f 329.6
+beep -f 415.3
+beep -f 523.2
+beep -f 587.4
+beep -f 784
+beep -f 698.4
+beep -f 587.4
+beep -f 329.6
+beep -f 329.6
+beep -f 415.3
+beep -f 523.2
+beep -f 587.4
+beep -f 784
+beep -f 698.4
+beep -f 440   -l 2000
+beep -f 370   -l 2000
+beep -f 440   -l 2000
+beep -f 440   -l 2000
+beep -f 293.7 -l 2000
+beep -f 440 -r 4
+}
+
+bip3(){
+echo "Super Mario"
+beep -f 660 -l 100
+beep -f 660 -l 100
+beep -f 660 -l 100
+beep -f 510 -l 100
+beep -f 660 -l 100
+beep -f 770 -l 100
+beep -f 380 -l 100
+beep -f 510 -l 100
+beep -f 380 -l 100
+beep -f 320 -l 100
+beep -f 440 -l 100
+beep -f 480 -l 80
+beep -f 450 -l 100
+beep -f 430 -l 100
+beep -f 380 -l 100
+beep -f 660 -l 80
+beep -f 760 -l 50
+beep -f 860 -l 100
+beep -f 700 -l 80
+beep -f 760 -l 50
+beep -f 660 -l 80
+beep -f 520 -l 80
+beep -f 580 -l 80
+beep -f 480 -l 80
+beep -f 510 -l 100
+beep -f 380 -l 100
+beep -f 320 -l 100
+beep -f 440 -l 100
+beep -f 480 -l 80
+beep -f 450 -l 100
+beep -f 430 -l 100
+beep -f 380 -l 100
+beep -f 660 -l 80
+beep -f 760 -l 50
+beep -f 860 -l 100
+beep -f 700 -l 80
+beep -f 760 -l 50
+beep -f 660 -l 80
+beep -f 520 -l 80
+beep -f 580 -l 80
+beep -f 480 -l 80
+beep -f 500 -l 100
+beep -f 760 -l 100
+beep -f 720 -l 100
+beep -f 680 -l 100
+beep -f 620 -l 150
+beep -f 650 -l 150
+beep -f 380 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 570 -l 100
+beep -f 500 -l 100
+beep -f 760 -l 100
+beep -f 720 -l 100
+beep -f 680 -l 100
+beep -f 620 -l 150
+beep -f 650 -l 200
+beep -f 1020 -l 80
+beep -f 1020 -l 80
+beep -f 1020 -l 80
+beep -f 380 -l 100
+beep -f 500 -l 100
+beep -f 760 -l 100
+beep -f 720 -l 100
+beep -f 680 -l 100
+beep -f 620 -l 150
+beep -f 650 -l 150
+beep -f 380 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 570 -l 100
+beep -f 585 -l 100
+beep -f 550 -l 100
+beep -f 500 -l 100
+beep -f 380 -l 100
+beep -f 500 -l 100
+beep -f 500 -l 100
+beep -f 500 -l 100
+beep -f 500 -l 100
+beep -f 760 -l 100
+beep -f 720 -l 100
+beep -f 680 -l 100
+beep -f 620 -l 150
+beep -f 650 -l 150
+beep -f 380 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 570 -l 100
+beep -f 500 -l 100
+beep -f 760 -l 100
+beep -f 720 -l 100
+beep -f 680 -l 100
+beep -f 620 -l 150
+beep -f 650 -l 200
+beep -f 1020 -l 80
+beep -f 1020 -l 80
+beep -f 1020 -l 80
+beep -f 380 -l 100
+beep -f 500 -l 100
+beep -f 760 -l 100
+beep -f 720 -l 100
+beep -f 680 -l 100
+beep -f 620 -l 150
+beep -f 650 -l 150
+beep -f 380 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 430 -l 100
+beep -f 500 -l 100
+beep -f 570 -l 100
+beep -f 550 -l 100
+beep -f 500 -l 100
+beep -f 380 -l 100
+beep -f 500 -l 100
+beep -f 500 -l 100
+beep -f 500 -l 100
+beep -f 500 -l 60
+beep -f 500 -l 80
+beep -f 500 -l 60
+beep -f 500 -l 80
+beep -f 580 -l 80
+beep -f 660 -l 80
+beep -f 500 -l 80
+beep -f 430 -l 80
+beep -f 380 -l 80
+beep -f 500 -l 60
+beep -f 500 -l 80
+beep -f 500 -l 60
+beep -f 500 -l 80
+beep -f 580 -l 80
+beep -f 660 -l 80
+beep -f 870 -l 80
+beep -f 760 -l 80
+beep -f 500 -l 60
+beep -f 500 -l 80
+beep -f 500 -l 60
+beep -f 500 -l 80
+beep -f 580 -l 80
+beep -f 660 -l 80
+beep -f 500 -l 80
+beep -f 430 -l 80
+beep -f 380 -l 80
+beep -f 660 -l 100
+beep -f 660 -l 100
+beep -f 660 -l 100
+beep -f 510 -l 100
+beep -f 660 -l 100
+beep -f 770 -l 100
+beep -f 380 -l 100
+}
+
+bip4(){
+echo "Queda link"
+beep -f 523.2
+beep -f 587.4 -l 1000
+beep -f 523.2
+beep -f 587.4 -l 1000
+beep -f 523.2
+beep -f 587.4 -l 1000
+}
+
+bip5(){
+echo "Sem link"
+beep -f 523.2
+beep -f 523.2
+}
+
+bip6(){
+echo "Restabelecimento link"
+beep -f 440 -r 4
+}
+
+case $1 in
+        bip1) bip1 ;;
+        bip2) bip2 ;;
+        bip3) bip3 ;;
+        bip4) bip4 ;;
+        bip5) bip5 ;;
+        bip6) bip6 ;;
+        *) echo "Você tem de entrar com um parâmetro válido" ;;
+esac
